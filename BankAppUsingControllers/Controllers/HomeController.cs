57a4efd017e5caea21119a98cc85b01db98087eb
debanjan444 <debanjanghosh075@gmail.com>
2026-06-28 12:20:50 +0530

    Add project files.

---
+++ BankAppUsingControllers/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using BankAppUsingControllers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankAppUsingControllers.Controllers
+{
+    public class HomeController : Controller
+    {
+        AccountDetails accountDetails = new AccountDetails()
+        {
+            accountNumber = 1001,
+            accountHolderName = "John Doe",
+            currentBalance = 1000
+        };
+        [Route("/")]    
+        public IActionResult Index()
+        {
+            return Ok("Welcome to the Bank App!");
+
+        }
+        [HttpGet]
+        [Route("/account-details")]
+        public IActionResult AccountDetails()
+        {
+            Response.StatusCode = 200;
+            return Json(accountDetails);
+        }
+        [HttpGet]
+        [Route("/account-statement")]
+        public IActionResult AccountStatement()
+        {
+            Response.StatusCode = 200;
+           return PhysicalFile(@"D:\\maa schedule\schdule.pdf", "application/pdf");
+        }
+        [HttpGet]
+        [Route("get-current-balance/{accountNumber:int?}")]
+        public IActionResult GetCurrentBalance()
+        {
+            int? myaccountNumber = Convert.ToInt32(Request.RouteValues["accountNumber"]);
+            if (!Request.RouteValues.ContainsKey("accountNumber"))
+            {
+                return NotFound("Account number should be supplied");
+
+            }
+            if(myaccountNumber != accountDetails.accountNumber)
+            {
+                return BadRequest("Account Number should be 1001");
+            }
+            
+                
+            return Ok($"Current Balance is {accountDetails.currentBalance}");
+           
+
+        }
+
+
+    }
+}
---
+++ BankAppUsingControllers/Models/AccountDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace BankAppUsingControllers.Models
+{
+    public class AccountDetails
+    {
+        public int accountNumber { get; set; }
+        public string accountHolderName { get; set; }
+        public int currentBalance { get; set; }
+    }
+}
